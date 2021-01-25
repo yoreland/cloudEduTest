26dc471cab530fffcd1d9ea071184827ddb8074d
Xia Ning <xianing@agora.io>
2021-01-25 23:42:25 +0800

    add web login test

--- src/test/java/web/LoginTest.java
+++ src/test/java/web/LoginTest.java
@@ -1,11 +1,21 @@
 package web;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
     @Test
-    public void ValidPasswordLogin(){
-        System.out.println("Hello!");
+    public void test(){
+        System.out.println("start test!");
+        System.setProperty("webdriver.chrome.driver", "/Users/xianing/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://solutions.agora.io/education/web_v2/");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div[2]/div[2]/div[1]/div/input")).sendKeys("automation1");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div[2]/div[2]/div[2]/div/input")).sendKeys("teacher");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div[2]/div[2]/div[4]/div/label[1]/span[1]/span[1]/input")).click();
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div[2]/div[2]/button")).click();
     }
 }
